b9c51d159da13dcb63e148b7bd81ae83eac3f65a
surat <surat@DESKTOP-SS5UEKF.lan>
2023-10-05 14:20:44 -0500

    added taskpage delete customer and locator methods

--- src/test/java/com/acti/testcase/TaskPageTests.java
+++ src/test/java/com/acti/testcase/TaskPageTests.java
@@ -1,5 +1,6 @@
 package com.acti.testcase;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TaskPageTests extends BaseTest{
@@ -13,11 +14,31 @@ public class TaskPageTests extends BaseTest{
 		enterPage.clickTaskMenu();
 		taskPage.clickAddNewButton();
 		taskPage.clickItemNewCustomer();
-		taskPage.enterCustomerName("Test Chandni");
-		taskPage.enterCustomerDescription("Automation");
+		taskPage.enterCustomerName("Automation");
+		taskPage.enterCustomerDescription("Selenium Webdriver");
 		taskPage.clickCustomerButton();
 		String msg = taskPage.getSuccessMessageText();
 		System.out.println(msg);
+		Assert.assertTrue(msg.contains("created"));
+	}
+	
+	@Test
+	public void testDeleteCustomer()
+	{
+		loginPage.enterUsername("admin");
+		loginPage.enterPassword("manager");
+		loginPage.clickLoginbutton();
+		String actual = enterPage.getUserLoggedInText();
+		Assert.assertTrue(actual.contains("John"));
+		enterPage.clickTaskMenu();
+		taskPage.enterCustomerType("Automation");
+		taskPage.clickSearchedCustomer();
+		taskPage.clickEditButton();
+		taskPage.clickActionButton();
+		taskPage.clickDeleteButton();
+		taskPage.clickDeletePermanently();
+		String msg = taskPage.getSuccessMessageText();
+		Assert.assertTrue(msg.contains("deleted"));
 	}
 	
 	
