c8351af5a64204c629f56332ba6361cfb619fcc0
surat <surat@DESKTOP-SS5UEKF.lan>
2023-10-05 14:21:57 -0500

    added code for sleep in helperlib class

--- src/main/java/com/acti/utils/HelperLib.java
+++ src/main/java/com/acti/utils/HelperLib.java
@@ -2,4 +2,15 @@ package com.acti.utils;
 
 public class HelperLib {
 
+	public static void sleep()
+	{
+		try
+		{
+			Thread.sleep(2000);
+		} 
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
